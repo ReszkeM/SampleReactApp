89a65aff8dcce8b24ef5e47c1d5af93e81bb1e83
Marek Reszke <MReszke@newforma.com>
2019-07-08 22:56:51 +0200

    setup tests

--- src/app/app-sample/App.test.tsx
+++ src/app/app-sample/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { create } from 'react-test-renderer';
+import configureStore from '../../store';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('it renders', () => {
+  const component = create(
+    <Provider store={configureStore()}>
+      <App />
+    </Provider>
+  );
+
+  expect(component).toBeTruthy();
+  expect(component.toJSON()).toMatchSnapshot();
 });
