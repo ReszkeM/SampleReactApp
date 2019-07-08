2e73ccbd5f7227a72eaa2c38d1286c99777dea88
Marek Reszke <MReszke@newforma.com>
2019-07-08 19:02:09 +0200

    update project structure

---
+++ src/app/containers/App/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from '../../../logo.svg';
+
+import './App.less';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <div class="alert alert-success" role="alert">
+            A simple success alert—check it out!
+          </div>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/App';
-import * as serviceWorker from './serviceWorker';
+import App from './app/containers/App/App';
+import * as serviceWorker from './app/utils/serviceWorker';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.less';
