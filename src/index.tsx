d8f62a854d940d5dc2341c74a7f2052f14dd8605
Marek Reszke <MReszke@newforma.com>
2019-07-09 20:54:52 +0200

    add react router dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from 'react-redux';
 
 import App from './app/app-sample/App';
@@ -11,7 +12,10 @@ import './assets/styles/index.less';
 
 ReactDOM.render(
   <Provider store={configureStore()}>
-    <App />
+    <Router>
+      <Route exact={true} path="/" component={App} />
+      <Route path="/sandbox" component={App} />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
